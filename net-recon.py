c28fa0e65b3f313a0c717a1477d7025084a0924f
k0fin <technicalsyn@gmail.com>
2018-07-31 16:51:54 -0400

    Improved LLDP system description parsing and added experimental LLDP TR-41 Location Identification address parsing

--- net-recon.py
+++ net-recon.py
@@ -172,24 +172,50 @@ class LLDP:
 
                 if packet.getlayer(Ether) and packet[Ether].dst == "01:80:c2:00:00:0e":
                     mac = packet[Ether].src
-                    raw_packet = list(str(packet[Raw])) # Load LLDP header
+                    raw_packet = list(str(packet[Raw]))
 
                     system_name_start = raw_packet.index('\x0a')
                     system_name_list = raw_packet[(system_name_start + 2):]
                     system_name = ''.join(system_name_list).rsplit('\x0c')[0]
+
                     hostname = system_name
-                    system_description = ''.join(raw_packet[71:]).rsplit('\x0e')[0].strip()
-                    mgt_addr_split = list(raw_packet[131:135])
+                    system_description_start = raw_packet.index('\x0c')
+                    system_description_list = raw_packet[(system_description_start + 2):]
+                    system_description = ''.join(system_description_list).rsplit('\x0e')[0]
+
+                    mgt_addr_start = list(''.join(system_description_list).rsplit('\x0e')[1].split()[1][2:])[0:4]
                     mgt_addr_list = []
 
-                    for mgt in mgt_addr_split:
+                    for mgt in mgt_addr_start:
                         octet = int(mgt.encode('hex'), 16)
                         mgt_addr_list.append(str(octet))
 
                     mgt_ipv4 = '.'.join(mgt_addr_list)
 
+                    teledata_start = raw_packet.index('\xfe')
+                    teledata_list = ''.join(raw_packet[teledata_start:]).lstrip('\xfe').split('\xfe')[1::]
+
+                    address = None
+
+                    if len(teledata_list) == 4:
+
+                        media_capabilities = teledata_list[0]
+                        network_policy = teledata_list[1]
+                        location_identification = teledata_list[2]
+                        extended_power = teledata_list[3]
+
+
+                        country = location_identification[8:10]
+                        state = location_identification[12:14]
+                        city = ''.join(location_identification[16:]).rsplit('\x06')[0]
+                        street = ''.join(location_identification[16:]).rsplit('\x06')[1].rsplit('\x13')[0].strip()
+                        number = ''.join(location_identification[16:]).rsplit('\x06')[1].split('\x13')[1][1:5].strip()
+                        unit = location_identification[-3:]
+
+                        address = '{} {} {} - {}, {} - {}'.format(number, street, unit, city, state, country)
+
                     if hostname not in self.keys['hosts'].keys():
-                        self.keys['hosts'].update({hostname:{'mac': mac, 'fingerprints': system_description, 'management_ipv4': mgt_ipv4, 'system_name': system_name, 'protocol': 'LLDP'}})
+                        self.keys['hosts'].update({hostname:{'mac': mac, 'fingerprints': system_description, 'management_ipv4': mgt_ipv4, 'system_name': system_name, 'tr41-location-id': address, 'protocol': 'LLDP'}})
 
 
         return self.keys
@@ -334,7 +360,7 @@ def create_report(rname, rkeys, quiet=False):
                 host_data_val = ', '.join(host_data_val)
 
             if not quiet:
-                print '  - {0:15} {1:10}'.format(host_data_key.upper(), host_data_val)
+                print '  - {0:20} {1:10}'.format(host_data_key.upper(), host_data_val)
 
         print ''
 
